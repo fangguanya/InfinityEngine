8e30e89c8d40d9946e52720a12385bd53faf6b8d
haolange <1824339952@qq.com>
2020-12-18 15:16:38 +0800

    [Code]Add EntitySystem.

---
+++ Engine/Source/Infinity.Core/EntitySystem/Component.cs
@@ -0,0 +1,39 @@
+﻿
+using InfinityEngine.Core.Object;
+
+namespace InfinityEngine.Core.EntitySystem
+{
+    public class Component : UObject
+    {
+        internal Entity OwnerEntity;
+
+        public Component()
+        {
+
+        }
+
+        public virtual void OnCreate() { }
+
+        public virtual void OnEnable() { }
+
+        protected virtual void OnTransformChanged() { }
+
+        public virtual void OnGameUpdate(float frameTime) { }
+
+        public virtual void OnEditorUpdate(float frameTime) { }
+
+        public virtual void OnDisable() { }
+
+        public virtual void OnRemove() { }
+
+        protected override void DisposeManaged()
+        {
+
+        }
+
+        protected override void DisposeUnManaged()
+        {
+
+        }
+    }
+}
