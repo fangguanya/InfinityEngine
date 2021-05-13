08f7e98252a2136e1fbe377ce69fc8d8631b3711
haolange <1824339952@qq.com>
2021-05-13 14:15:35 +0800

    [Code]Add RenderComponent.

---
+++ Engine/Source/Infinity.Game/Component/StaticMeshComponent.cs
@@ -0,0 +1,36 @@
+﻿using InfinityEngine.Core.Object;
+using InfinityEngine.Game.ActorSystem;
+
+namespace InfinityEngine.Game.Component
+{
+    public class FStaicMeshProxy : UObject
+    {
+        protected override void Disposed()
+        {
+
+        }
+    }
+
+    public class UStaticMeshComponent : UComponent
+    {
+        public UStaticMeshComponent()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+
+        }
+
+        public override void OnDisable()
+        {
+
+        }
+    }
+}
