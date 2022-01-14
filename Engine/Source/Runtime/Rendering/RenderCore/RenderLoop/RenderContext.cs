7da57ecfb0dcc3d8eda392e93cc0d1ad8c2cb82d
haolange <1824339952@qq.com>
2022-01-14 13:02:38 +0800

    update internal

--- Engine/Source/Runtime/Rendering/RenderCore/RenderLoop/RenderContext.cs
+++ Engine/Source/Runtime/Rendering/RenderCore/RenderLoop/RenderContext.cs
@@ -10,8 +10,8 @@ namespace InfinityEngine.Rendering.RenderLoop
         public ulong copyFrequency => m_Context.copyFrequency;
         public ulong computeFrequency => m_Context.computeFrequency;
         public ulong graphicsFrequency => m_Context.graphicsFrequency;
-        public FRHITexture backBuffer => m_SwapChain.backBuffer;
-        public FRHIRenderTargetView backBufferView => m_SwapChain.backBufferView;
+        internal FRHITexture backBuffer => m_SwapChain.backBuffer;
+        internal FRHIRenderTargetView backBufferView => m_SwapChain.backBufferView;
 
         private FRHIContext m_Context;
         private FRHISwapChain m_SwapChain;
